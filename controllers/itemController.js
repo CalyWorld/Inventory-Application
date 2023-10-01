5bd4fe879b8a297d2a2e85c9c80ed71497fabae9
CalyWorld <obinnaanizoba5@gmail.com>
2023-10-02 06:29:40 +0800

    implemented item edit functionality

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -36,6 +36,7 @@ exports.item_create_post = [
       category: req.body.category,
     });
     if (!errors.isEmpty()) {
+      const category_list = await Category.find().exec();
       res.render("item_form", {
         title: "Create an Item",
         category_list: category_list,
@@ -72,9 +73,58 @@ exports.item_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  res.send("GET Form Submission on Update");
+  const [category_item, category_list] = await Promise.all([
+    Item.findById(req.params.itemId),
+    Category.find().exec(),
+  ]);
+  res.render("item_form", {
+    title: "Create an Item",
+    category_list: category_list,
+    item: category_item,
+  });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-  res.send("POST Form Submission on Update");
-});
+exports.item_update_post = [
+  body("name", "name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 5 })
+    .escape(),
+  body("price", "Price must not be empty").isInt({ gt: 0 }),
+  body("quantity", "Quantity must not be empty").isInt({ gt: 0 }),
+  body("imageUrl", "URL must not be empty").trim().isLength({ min: 1 }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const category_list = await Category.find().exec();
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      imageUrl: req.body.imageUrl,
+      category: req.body.category,
+      _id: req.params.itemId,
+    });
+    if (!errors.isEmpty()) {
+      const category_list = await Category.find().exec();
+      res.render("item_form", {
+        title: "Create an Item",
+        category_list: category_list,
+        item: item,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedItem = await Item.findByIdAndUpdate(
+        req.params.itemId,
+        item,
+        {},
+      );
+      res.redirect(updatedItem.url);
+      console.log(category_list, item);
+    }
+  }),
+];
